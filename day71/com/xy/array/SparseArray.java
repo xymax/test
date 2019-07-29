2254b23832129cc6d979163b4b1d4ffa6d21f725
xymax <1437310304@qq.com>
2019-07-29 23:24:24 +0800

    稀疏数组

---
+++ day71/com/xy/array/SparseArray.java
@@ -0,0 +1,79 @@
+package com.xy.array;
+
+public class SparseArray {
+    public static void main(String[] args) {
+        //创建一个二维数组11*11  0表示没有棋子 1表示黑子 2表示白子
+        int chessArr1[][] = new int[11][11];
+        chessArr1[1][2] = 1;
+        chessArr1[2][3] = 2;
+        //输出原始的二维数组
+        System.out.println("原始二维数组");
+        for (int[] row : chessArr1) {
+            for (int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+
+
+        //先将二维数组转稀疏数组的思路
+        //1.先进行遍历二维数组，得到非0数据的个数
+        int sum = 0;
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (chessArr1[i][j] != 0) {
+                    sum++;
+                }
+            }
+        }
+
+
+// 2. 创建对应的稀疏数组
+        int sparseArr[][] = new int[sum + 1][3];
+        // 给稀疏数组赋值
+        sparseArr[0][0] = 11;
+        sparseArr[0][1] = 11;
+        sparseArr[0][2] = sum;
+
+        // 遍历二维数组，将非0的值存放到 sparseArr中
+        int count = 0; //count 用于记录是第几个非0数据
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (chessArr1[i][j] != 0) {
+                    count++;
+                    sparseArr[count][0] = i;
+                    sparseArr[count][1] = j;
+                    sparseArr[count][2] = chessArr1[i][j];
+                }
+            }
+        }
+
+        // 输出稀疏数组的形式
+        System.out.println();
+        System.out.println("得到稀疏数组为~~~~");
+        for (int i = 0; i < sparseArr.length; i++) {
+            System.out.printf("%d\t%d\t%d\t\n", sparseArr[i][0], sparseArr[i][1], sparseArr[i][2]);
+        }
+        System.out.println();
+
+
+        //将稀疏数组恢复为原始的数组
+
+        //第一步：先读取系数数组的第一行，根据第一行的数据，创建原始的二维数组
+        int chessArr2[][] = new int[sparseArr[0][0]][sparseArr[0][1]];
+        //第二部，将读取稀疏数组后面几行的数据，并赋值给二维数组即可
+        for (int i = 1; i < sparseArr.length; i++) {
+            chessArr2[sparseArr[i][0]][sparseArr[i][1]] = sparseArr[i][2];
+        }
+        //恢复后的二维数组
+        System.out.println();
+        System.out.println("恢复后的二维数组");
+
+        for (int[] row : chessArr2) {
+            for (int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+    }
+}
