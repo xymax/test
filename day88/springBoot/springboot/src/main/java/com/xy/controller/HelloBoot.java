b57abc51dc6284f0b41ac0b3b857a5d7ea1b1f2c
xymax <1437310304@qq.com>
2019-08-19 23:45:44 +0800

    spring boot

---
+++ day88/springBoot/springboot/src/main/java/com/xy/controller/HelloBoot.java
@@ -0,0 +1,18 @@
+package com.xy.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloBoot {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+}
---
+++ day88/springBoot/springboot/src/main/java/com/xy/controller/MyBootApplication.java
@@ -0,0 +1,18 @@
+package com.xy.controller;
+
+import com.xy.controller.HelloBoot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootApplication {
+
+/*    public static void main(String[] args) {
+        SpringApplication.run(HelloBoot.class,args);
+    }*/
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloBoot.class,args);
+    }
+}
